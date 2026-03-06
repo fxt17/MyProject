3815bc9a64ba3ec765ce047e777261eb72e25e73
fxt17 <112749058+fxt17@users.noreply.github.com>
2026-03-06 09:55:47 +0800

    Update msg_queue.c

---
+++ common/msg_queue.c
@@ -0,0 +1,18 @@
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include "msg_queue.h"
+
+int msg_create()
+{
+    key_t key = ftok("/tmp", 1);
+
+    int msgid = msgget(key, IPC_CREAT | 0666);
+
+    if(msgid < 0)
+    {
+        perror("msgget");
+    }
+
+    return msgid;
+}
